f1dabce6f6f516f321f05c7ca90ad783c4da5f88
Adam Price <komidore64@gmail.com>
2013-10-31 11:52:14 -0400

    generate.rb - default url; help output
    - changing the default url
    - optparse already takes care of -h and --help

--- bin/generate.rb
+++ bin/generate.rb
@@ -3,20 +3,15 @@
 require 'optparse'
 $:.unshift(File.expand_path("../../lib", __FILE__))
 
-options = {:katello_apidoc_url => "http://localhost:3000/apidoc"}
+options = {:katello_apidoc_url => "http://localhost:3000/katello/apidoc"}
 
 OptionParser.new do |opts|
-  opts.banner = <<BANNER
-Script for generating API bindings for Katello API from Apipie docs.
-BANNER
+  opts.banner = "Script for generating API bindings for Katello API from Apipie docs."
+
   opts.on("-u", "--url KATELLO_APIDOC_URL", "By default #{options[:katello_apidoc_url]}") do |val|
     options[:katello_apidoc_url] = val
   end
 
-  opts.on("-h", "--help") do
-    puts opts
-    exit
-  end
 end.parse!
 
 require 'katello_api/generator/base.rb'
